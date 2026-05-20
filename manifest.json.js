c4fd3b2111c2f666f8e9b1c1c3fd5e4568a85076
sk47td <132130518+sk47td@users.noreply.github.com>
2026-05-20 18:30:30 +0530

    Add files via upload

---
+++ manifest.json.js
@@ -0,0 +1,20 @@
+{
+  "short_name": "PZ Stock",
+  "name": "Phonezone Stock Manager",
+  "description": "Live cloud mobile cover stock tracking application",
+  "icons": [
+    {
+      "src": "app-icon.png",
+      "type": "image/png",
+      "sizes": "512x512",
+      "purpose": "any maskable"
+    }
+  ],
+  "start_url": "./index.html",
+  "background_color": "#f8fafc",
+  "theme_color": "#2563eb",
+  "display": "standalone",
+  "orientation": "portrait"
+}
+
+
